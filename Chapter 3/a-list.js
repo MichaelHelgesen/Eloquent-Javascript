523bf72d5d600b41eb2a77f280e8b2909b97772b
Michael Helgesen <michaelhelgesen02@gmail.com>
2019-10-22 22:48:24 +0200

    Doing exercises

--- Chapter 3/a-list.js
+++ Chapter 3/a-list.js
@@ -1,3 +1,25 @@
+// NOT MY SOLUTION
+// step parameter is optional
+// if step is not passed in, 
+// and start is less than or equal to end, 
+// then step = 1, else step = -1
+function range(start, end, step = start <= end ? 1 : -1) {
+    let result = [];
+    // loop iterates up for positive step values
+    // and iterates down for negative step values
+    for (let i = start; step >= 0 ? i <= end : i >= end; i+=step) {
+      result.push(i);
+    }
+    return result;
+  }
+
+const sum = (array) => array.length === 0 ? 0 : array[0] + sum(array.slice(1))
+
+console.log(sum(range(1, 10)));
+
+
+// MY SOLUTION - NON SHORTENED
+/*
 const range = (start, end) => {
     if (end === start) {
         return [start];
@@ -9,15 +31,52 @@ const range = (start, end) => {
 };
 
 const sum = (array) => {
-    let count = 0;
-    if (count === array.length) {
-        return array[0];
+    if (array.length === 0) {
+        return 0;
     } else {
-        count++
-        array[0] += sum(array[count]);  
-    }
-    
+        //console.log(array);
+        var recursion = array[0] + sum(array.slice(1));
+        console.log(recursion);
+        //console.log(array);
+        return recursion;  
+    } 
 }
+*/
+
+// What happens here is that every recursion call gets saved to the stack. They don´t start returning after
+// reaching the base case. So when the array has no length, the first result coming back to us is 10 + 0.
+// Then it continues down the stack, all the way down to when the array was full, giving us 54 + 1
+
+// This is how its summed up:
+// 10 [10]
+// 19 [10, 9]
+// 27 [8, 9, 10]
+// 34 [7, 8, 9, 10]
+// 40
+// 45
+// 49
+// 52
+// 54
+// 55
 
 
-console.log(sum(range(1, 10)));
+// [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+// [ 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+// [ 3, 4, 5, 6, 7, 8, 9, 10 ]
+// [ 4, 5, 6, 7, 8, 9, 10 ]
+// [ 5, 6, 7, 8, 9, 10 ]
+// [ 6, 7, 8, 9, 10 ]
+// [ 7, 8, 9, 10 ]
+// [ 8, 9, 10 ]
+// [ 9, 10 ]
+// [ 10 ]
+// [ 10 ]
+// [ 9, 10 ]
+// [ 8, 9, 10 ]
+// [ 7, 8, 9, 10 ]
+// [ 6, 7, 8, 9, 10 ]
+// [ 5, 6, 7, 8, 9, 10 ]
+// [ 4, 5, 6, 7, 8, 9, 10 ]
+// [ 3, 4, 5, 6, 7, 8, 9, 10 ]
+// [ 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+// [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
---
+++ Chapter 3/reversing-an-array.js
@@ -0,0 +1,21 @@
+
+// take an array as argument
+// create a new array
+// return the new array with the values from the one passed in in reverse.
+const reverseArray = (array) => {
+    let reverseArray = [];
+    for (i = array.length-1; i >= 0; i--) {
+        reverseArray.push(array[i]);
+    }
+    return reverseArray;
+}
+
+const reverseArrayInPlace = (array) => {
+    // base case
+    
+    return array.push("l")
+    
+    
+}
+
+console.log(reverseArrayInPlace([1,2,3,4,"w"]));
